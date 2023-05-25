e5ba9cebb42c51cf1a4467db7d3fae0b4bf6a796
rajputpihu <102144263+rajputpihu@users.noreply.github.com>
2023-05-25 11:46:03 +0530

    onlinebookstore

---
+++ admin/includes/config.php
@@ -0,0 +1,4 @@
+<?php 
+$conn=mysqli_connect("localhost","root","","book store")or die("Can't Connect...");
+	
+?>
---
+++ includes/footer.inc.php
@@ -0,0 +1,3 @@
+<div id="footer-wrap">
+	<p id="legal">(c) 2017 OurSite. Design by <a href="http://www.MyBookStore.com"> My Book Store @ Gunjan</a>.</p>
+	</div>
